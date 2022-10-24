e011aa5b695edc96471ad8f6894754df20be733e
Mia <i5heu@i5h.eu>
2022-10-24 19:11:18 +0200

    add frontend

---
+++ src/default.ts
@@ -0,0 +1,7 @@
+export type Store = {
+  javascript: string;
+};
+
+export const defaults: Store = {
+  javascript: "test('hello world')",
+};
---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import { editor } from "monaco-editor";
+import { Store, defaults } from "./default";
+
+const store: Store = getStorageFromBrowser()
+  ? getStorageFromBrowser()
+  : defaults;
+
+const editorInstance = editor.create(document.getElementById("editor"), {
+  value: store.javascript,
+  language: "javascript",
+  theme: "vs-dark",
+});
+window.onresize = function () {
+  editorInstance.layout();
+};
+
+console.log("Hello World");
+
+let loading = false;
+editorInstance.getModel().onDidChangeContent((e) => {
+  if (loading) return;
+  loading = true;
+  setTimeout(() => {
+    saveStorageInBrowser();
+    loading = false;
+  }, 1000);
+});
+
+document.getElementById("save").addEventListener("click", () => {
+  const blob = new Blob([JSON.stringify(store)], { type: "text/plain" });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "store.json";
+  a.click();
+});
+
+document.getElementById("import").addEventListener("click", () => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".json";
+  input.onchange = (e) => {
+    const file = (e.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = e.target.result as string;
+      const store = JSON.parse(text);
+      editorInstance.setValue(store.javascript);
+    };
+    reader.readAsText(file);
+  };
+  input.click();
+});
+
+function saveStorageInBrowser() {
+  localStorage.setItem("store", JSON.stringify(store));
+}
+
+function getStorageFromBrowser() {
+  const store = localStorage.getItem("store");
+  if (store) return JSON.parse(store);
+  return null;
+}
