64c74a4f9d7d699063772250295e3052cba681e5
i5heu <i5heu@i5h.eu>
2022-10-29 14:54:42 +0000

    working example for send and receive

--- backend/agent/agent.go
+++ backend/agent/agent.go
@@ -3,50 +3,104 @@ package agent
 import (
 	"fmt"
 	"reflect"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/traefik/yaegi/interp"
 	"github.com/traefik/yaegi/stdlib"
 )
 
 type Data struct {
-	Msg string `json:"msg"`
+	SenderID uuid.UUID
+	Msg      string `json:"msg"`
 }
 
-func New(code string) {
+type Info struct {
+	AgentID uuid.UUID
+	Seeds   []uuid.UUID
+}
 
-	fmt.Println("111")
+type Agent struct {
+	NodeID uuid.UUID `json:"node_id"`
+	Chanel chan Data
+}
+
+type agentCtx struct {
+	info   Info
+	agents *map[uuid.UUID]Agent
+}
+
+func New(code string, agentData Agent, agents *map[uuid.UUID]Agent, seeds []uuid.UUID) {
+	info := Info{
+		AgentID: agentData.NodeID,
+		Seeds:   seeds,
+	}
+	ctx := agentCtx{
+		info:   info,
+		agents: agents,
+	}
+
 	i := interp.New(interp.Options{})
 
 	i.Use(stdlib.Symbols)
 	i.Use(
 		map[string]map[string]reflect.Value{
 			"custom/custom": {
-				"Data": reflect.ValueOf((*Data)(nil)),
-				"Send": reflect.ValueOf(nodeSend),
+				"Data":    reflect.ValueOf((*Data)(nil)),
+				"Info":    reflect.ValueOf((*Info)(nil)),
+				"GetInfo": reflect.ValueOf(info.nodeInfo),
+				"Send":    reflect.ValueOf(ctx.nodeSend),
 			},
 		})
 
 	_, err := i.Eval(code)
 	if err != nil {
-		fmt.Println("ERRRROLL", err)
+		fmt.Println("CompilerError:", err)
 	}
 
-	fmt.Println("Done xxxxxxxx Done")
-
-	v, err := i.Eval("Receive")
+	agentFuncReceive, err := i.Eval("Receive")
 	if err != nil {
 		fmt.Println(err)
 	}
+	receive := agentFuncReceive.Interface().(func(Data))
+	go ReceiveWorker(agentData.Chanel, receive)
 
-	bar := v.Interface().(func(Data))
+	agentFuncInit, err := i.Eval("Init")
+	if err != nil {
+		fmt.Println(err)
+	}
+	init := agentFuncInit.Interface().(func())
+	init()
 
-	bar(Data{Msg: "Hello World"})
 }
 
-func nodeSend(data interface{}) {
-	fmt.Println("nodeSend", data)
+func ReceiveWorker(dataChan chan Data, receive func(Data)) {
+	count := 0
+	unixTime := time.Now().Unix()
+
+	for data := range dataChan {
+		count++
+
+		if unixTime < time.Now().Unix() {
+			fmt.Println("ReceiveWorker", count)
+			count = 0
+			unixTime = time.Now().Unix()
+		}
+
+		receive(data)
+	}
 }
 
-func interfaceToString(data Data) {
-	fmt.Println("interfaceToString", data)
+func (i Info) nodeInfo() Info {
+	return Info{
+		AgentID: i.AgentID,
+		Seeds:   i.Seeds,
+	}
+}
+
+func (ctx agentCtx) nodeSend(target uuid.UUID, msg string) {
+	(*ctx.agents)[target].Chanel <- Data{
+		SenderID: ctx.info.AgentID,
+		Msg:      msg,
+	}
 }
--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"main/agent"
 	"main/helper"
 	"main/manager"
 	"net/http"
+	"os"
+	"syscall"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type HandlerChanel struct {
@@ -31,6 +36,14 @@ type RunSimulator struct {
 }
 
 func main() {
+	var err error
+	// get the file descriptor for the log file
+	// f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	log.Fatalf("error opening file: %v", err)
+	// }
+	redirectStderr(os.Stdout)
+
 	runSimulatorInst := &HandlerChanel{
 		MsgPerSecondChanel: make(chan int, 300),
 		IsoStore:           manager.NewIsolateStore(),
@@ -42,7 +55,7 @@ func main() {
 	fs := http.FileServer(http.Dir("../dist"))
 	http.Handle("/", fs)
 
-	err := http.ListenAndServe(":3333", nil)
+	err = http.ListenAndServe(":3333", nil)
 	if err != nil {
 		fmt.Println("ListenAndServe: ", err)
 	} else {
@@ -60,7 +73,39 @@ func (h *HandlerChanel) runSimulator(w http.ResponseWriter, r *http.Request) {
 		helper.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"message": err.Error()})
 	}
 
+	agents := make(map[uuid.UUID]agent.Agent)
+
+	var simpleSeed uuid.UUID
+
 	for i := 0; i < data.Config.Nodes; i++ {
-		go agent.New(data.Code)
+		agentId := uuid.New()
+		agents[agentId] = agent.Agent{
+			NodeID: agentId,
+			Chanel: make(chan agent.Data, 100),
+		}
+
+		if simpleSeed == uuid.Nil {
+			simpleSeed = agentId
+		}
+	}
+
+	for _, agentData := range agents {
+		go agent.New(data.Code, agentData, &agents, []uuid.UUID{agents[simpleSeed].NodeID})
+	}
+
+	// close channels after timeout
+	go func() {
+		time.Sleep(time.Duration(data.Config.Timeout) * time.Second)
+		fmt.Println("Timeout... closing channels")
+		for _, agentData := range agents {
+			close(agentData.Chanel)
+		}
+	}()
+}
+
+func redirectStderr(f *os.File) {
+	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to redirect stderr to file: %v", err)
 	}
 }
