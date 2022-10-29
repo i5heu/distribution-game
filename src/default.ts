96c965feb26ee322695c76446d147c332bbd3477
i5heu <i5heu@i5h.eu>
2022-10-29 13:21:45 +0000

    update defaults

--- src/default.ts
+++ src/default.ts
@@ -2,6 +2,4 @@ export type Store = {
   golang: string;
 };
 
-export const defaults: Store = {
-  golang: "test('hello world');",
-};
+export const defaults: Store = { "golang":"package main\n\nimport (\n    \"custom/custom\"\n\t\"strconv\"\n\t\"fmt\"\n    \"time\"\n)\n\nvar count = 0\n\nfunc test(){\n    for {\n        count++\n    \n time.Sleep(1 * time.Second)\n    }\n}\n\nfunc Receive(data custom.Data){\n    count++\n    custom.Send(data)\t\n}"}
