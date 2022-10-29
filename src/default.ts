cd0b5401f81037a229a9dbc6590f3ec6522ab4b1
i5heu <i5heu@i5h.eu>
2022-10-29 17:36:22 +0000

    update example code

--- src/default.ts
+++ src/default.ts
@@ -2,4 +2,6 @@ export type Store = {
   golang: string;
 };
 
-export const defaults: Store = { "golang":"package main\n\nimport (\n    \"custom/custom\"\n\t\"strconv\"\n\t\"fmt\"\n    \"time\"\n)\n\nvar count = 0\n\nfunc test(){\n    for {\n        count++\n    \n time.Sleep(1 * time.Second)\n    }\n}\n\nfunc Receive(data custom.Data){\n    count++\n    custom.Send(data)\t\n}"}
+export const defaults: Store = {
+  "golang":"package main\n\nimport (\n    \"dg/dg\"\n    \"time\"\n    \"fmt\"\n)\n\nvar Info dg.Info\nvar terminate bool = false\n\nfunc Receive(data dg.Data){\n    //fmt.Println(\"DATA\", data, \"ME\", Info)\n}\n\nfunc Init(){\n    Info = dg.GetInfo()\n\n    for {\n        if terminate {\n            \n            break\n        }\n\t\t\ttime.Sleep(time.Second)\n    dg.Send(Info.Seeds[0], \"helllo\")\n\t\t}\n\n}\n\nfunc Close(){\n    terminate = true\n}"
+};
